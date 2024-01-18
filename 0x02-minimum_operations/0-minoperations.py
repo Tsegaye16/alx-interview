7ea2261fbaaec3bbda8bf67e0c8adc95231b2cd3
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-18 22:05:01 +0000

    third day of alx interview

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""In a text file, there is a single character H.
+ Your text editor can execute only two operations
+ in this file: Copy All and Paste. Given a number
+n, write a method that calculates the fewest number
+of operations needed to result in exactly n H
+characters in the file."""
+
+
+def minOperations(n: int) -> :
+    """
+    calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file
+    """
+    if n < 0:
+        return 0
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    for i in range(2, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+                dp[i] = min(dp[i], dp[i // j] + j)
+    return dp[n] if dp[n] != float('inf') else 0
