975893a99ad10c8eaf1b486c43f8f48d0d840cd7
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-01 18:46:54 +0000

    4th day of inteview

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,29 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
 UTF-8 Validation
 """
 
 
 def validUTF8(data):
-     """
+    """
     data: list of integers
     Return: True if data is a valid UTF-8
     encoding, else return False
     """
-    num_leading_one = 0
+    byte_count = 0
 
-    for byte in data:
-        if num_leading_one > 0:
-            if (byte >> 6) != 0b10:
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
-            num_leading_one -= 1
         else:
-            while (byte >> (7 - num_leading_one)) & 1 == 1:
-                num_leading_one += 1
-
-            if num_leading_one == 1 or num_leading_one > 4:
+            if i >> 6 != 0b10:
                 return False
-    return num_leading_one == 0
+            byte_count -= 1
+    return byte_count == 0
